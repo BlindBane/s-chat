e66948dbc01bfaf70f747dbc504f40061931e13b
Aidan Bane <aidanfbane@gmail.com>
2017-07-08 17:15:48 -0700

    (doc) Init webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  devtool: "cheap-eval-source-map",
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  devServer: {
+    publicPath: "/dist/",
+    contentBase: [path.join(__dirname, "public"), path.join(__dirname, "dist")]
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+};
